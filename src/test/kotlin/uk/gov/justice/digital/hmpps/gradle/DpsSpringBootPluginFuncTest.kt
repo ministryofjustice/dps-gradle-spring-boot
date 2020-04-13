6860b33b2b538ca7dc6ad1f5f12007858c21a973
Mike Halma <mike.halma@digital.justice.gov.uk>
2020-04-13 12:33:41 +0100

    DT-726 Add running jar to bootJar test

--- src/test/kotlin/uk/gov/justice/digital/hmpps/gradle/DpsSpringBootPluginFuncTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/gradle/DpsSpringBootPluginFuncTest.kt
@@ -4,34 +4,66 @@ import org.assertj.core.api.Assertions.assertThat
 import org.gradle.testkit.runner.BuildResult
 import org.gradle.testkit.runner.GradleRunner
 import org.gradle.testkit.runner.TaskOutcome
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.io.TempDir
+import java.io.BufferedReader
 import java.io.File
+import java.io.InputStreamReader
+import java.net.URL
 import java.nio.file.Files
-import java.nio.file.StandardOpenOption
 
 const val PROJECT_NAME = "spring-boot-project"
+const val MAIN_CLASS = "Application"
 
 class DpsSpringBootPluginFuncTest {
 
   @TempDir
   lateinit var projectDir: File
 
+  var jarProcess: Process? = null
+
   @BeforeEach
   fun `Create source files`() {
     makeBuildScript()
     makeSrcFile()
     makeSettingsScript()
+    makeSpringPropertiesFile()
+  }
+
+  @AfterEach
+  fun `End running jar`() {
+    jarProcess?.destroyForcibly()
   }
 
   @Test
-  fun `Can create a Spring Boot jar`() {
+  fun `Can create and start a Spring Boot jar`() {
     val result = buildProject("bootJar")
 
     assertThat(result.task(":bootJar")?.outcome).isEqualTo(TaskOutcome.SUCCESS)
     val jar = File(projectDir, "build/libs/$PROJECT_NAME.jar")
     assertThat(jar.exists()).isTrue()
+
+    jarProcess = runJar(jar)
+    val healthResponse = URL("http://localhost:8080/actuator/health").readText()
+    assertThat(healthResponse).isEqualTo("""{"status":"UP"}""")
+  }
+
+  private fun runJar(jar: File): Process {
+    val process = ProcessBuilder("java", "-jar", jar.absolutePath).start()
+    val outputReader = BufferedReader(InputStreamReader(process.inputStream))
+    var startedOk = false
+    while(true) {
+      val line = outputReader.readLine() ?: break
+      println(line)
+      if (line.contains("Started ${MAIN_CLASS}Kt")) {
+        startedOk = true
+        break
+      }
+    }
+    assertThat(startedOk).isTrue().withFailMessage("Unable to start the Spring Boot jar")
+    return process
   }
 
   private fun makeSrcFile() {
@@ -45,7 +77,7 @@ class DpsSpringBootPluginFuncTest {
         import org.springframework.boot.runApplication
   
         @SpringBootApplication
-        class Application
+        open class $MAIN_CLASS
   
         fun main(args: Array<String>) {
           runApplication<Application>(*args)
@@ -54,6 +86,7 @@ class DpsSpringBootPluginFuncTest {
     Files.writeString(srcFile.toPath(), srcFileScript)
   }
 
+  // TODO DT-727 Next task is to move everything below the plugins section to the plugin and make sure that these tests still pass
   private fun makeBuildScript() {
     val buildFile = File(projectDir, "build.gradle.kts")
     val buildScript = """
@@ -77,6 +110,7 @@ class DpsSpringBootPluginFuncTest {
           implementation("org.jetbrains.kotlin:kotlin-reflect")
         
           implementation("org.springframework.boot:spring-boot-starter-web:2.2.6.RELEASE")
+          implementation("org.springframework.boot:spring-boot-starter-actuator:2.2.6.RELEASE")
         }
   
       """.trimIndent()
@@ -89,6 +123,21 @@ class DpsSpringBootPluginFuncTest {
         rootProject.name = "$PROJECT_NAME"
       """.trimIndent()
     Files.writeString(settingsFile.toPath(), settingsScript)
+  }
+
+  private fun makeSpringPropertiesFile() {
+    val propertiesDir = File(projectDir, "src/main/kotlin/resource")
+    propertiesDir.mkdirs()
+    val propertiesFile = File(projectDir, "application.yaml")
+    val properties = """
+        management:
+          endpoints:
+            web:
+              base-path: /
+              exposure:
+                include: 'health'
+      """.trimIndent()
+    Files.writeString(propertiesFile.toPath(), properties)
   }
 
   private fun buildProject(task: String): BuildResult {
