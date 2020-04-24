f57a0f354a86371c6b0d5052575b03a5fad414ac
Mike Halma <mike.halma@digital.justice.gov.uk>
2020-04-24 12:18:03 +0100

    DT-727 Refactor

--- src/test/kotlin/uk/gov/justice/digital/hmpps/gradle/DependencySuppressionFuncTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/gradle/DependencySuppressionFuncTest.kt
@@ -1,8 +1,9 @@
 package uk.gov.justice.digital.hmpps.gradle
 
 import org.assertj.core.api.Assertions.assertThat
+import org.gradle.internal.impldep.org.codehaus.plexus.util.FileUtils
 import org.gradle.testkit.runner.TaskOutcome
-import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.io.TempDir
 import java.io.File
@@ -12,35 +13,19 @@ private const val ADDITIONAL_SUPPRESSION_FILENAME = "jose-nimbus-suppression.xml
 
 class DependencySuppressionFuncTest {
 
-  companion object {
+  @TempDir
+  lateinit var projectDir: File
 
-    @TempDir
-    @JvmStatic
-    lateinit var projectDir: File
-
-    @BeforeAll
-    @JvmStatic
-    fun `Create and run project with additional dependency check suppression file`() {
-      makeAdditionalSuppressionFile(projectDir)
-      makeProject(javaProjectDetails(projectDir).copy(buildScript = """
-          plugins {
-            id("uk.gov.justice.digital.hmpps.gradle.DpsSpringBoot") version "0.0.1-SNAPSHOT"
-          }
-           
-          dependencies {
-            implementation("org.springframework.boot:spring-boot-starter-security")   // CVEs from this lib are suppressed in the plugin's DEPENDENCY_SUPPRESSION_FILENAME
-            implementation("com.nimbusds:nimbus-jose-jwt:7.8")                        // CVEs from this lib are suppressed in ADDITIONAL_SUPPRESSION_FILENAME
-          }
-          
-          dependencyCheck {
-            suppressionFiles.add("$projectDir/$ADDITIONAL_SUPPRESSION_FILENAME")      // We only need the project directory here because the GradleRunner's working directory is not the same as the project directory.  Normally we run Gradle tasks from the project directory so the files will not need the directory prefix.
-          }
-      """.trimIndent()))
-    }
+  @AfterEach
+  fun `Delete project`() {
+    FileUtils.cleanDirectory(projectDir)
   }
 
   @Test
   fun `Additional suppression file should be applied and not override plugin suppression file`() {
+    makeAdditionalSuppressionFile(projectDir)
+    makeProjectWithNimbusVulnerability(projectDir)
+
     val result = buildProject(projectDir, "dependencyCheckAnalyze")
     assertThat(result.task(":dependencyCheckAnalyze")?.outcome).isEqualTo(TaskOutcome.SUCCESS)
   }
@@ -61,4 +46,21 @@ private fun makeAdditionalSuppressionFile(projectDir: File) {
       </suppress>
     </suppressions>
   """.trimIndent())
+}
+
+private fun makeProjectWithNimbusVulnerability(projectDir: File) {
+  makeProject(javaProjectDetails(projectDir).copy(buildScript = """
+        plugins {
+          id("uk.gov.justice.digital.hmpps.gradle.DpsSpringBoot") version "0.0.1-SNAPSHOT"
+        }
+         
+        dependencies {
+          implementation("org.springframework.boot:spring-boot-starter-security:2.2.6.RELEASE")   // CVEs from this lib are suppressed in the plugin's DEPENDENCY_SUPPRESSION_FILENAME
+          implementation("com.nimbusds:nimbus-jose-jwt:7.8")                        // CVEs from this lib are suppressed in ADDITIONAL_SUPPRESSION_FILENAME
+        }
+        
+        dependencyCheck {
+          suppressionFiles.add("$projectDir/$ADDITIONAL_SUPPRESSION_FILENAME")      // We only need the project directory here because the GradleRunner's working directory is not the same as the project directory.  Normally we run Gradle tasks from the project directory so the files will not need the directory prefix.
+        }
+    """.trimIndent()))
 }
