b13fa78cd2694038e9e9b4074bcc42f0e07e4769
Mike Halma <mike.halma@digital.justice.gov.uk>
2020-12-10 16:15:40 +0000

    DT-1398 Force versions of tomcat and hibernate to avoid CVEs

--- src/main/kotlin/uk/gov/justice/digital/hmpps/gradle/pluginmanagers/DependencyManagementPluginManager.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/gradle/pluginmanagers/DependencyManagementPluginManager.kt
@@ -10,6 +10,13 @@ class DependencyManagementPluginManager(override val project: Project) : PluginM
 
   override fun configure() {
     applyDependencyManagementBom(project)
+    forceVersions(project)
+  }
+
+  // TODO Remove these forced versions once they are pulled in automatically
+  private fun forceVersions(project: Project) {
+    project.extensions.extraProperties["tomcat.version"] = "9.0.40" // Required to avoid version 9.0.39 introduced by spring-boot-starter-web 2.4.0 (CVE-2020-17527)
+    project.extensions.extraProperties["hibernate.version"] = "5.4.24.Final" // Required to avoid 5.4.23.Final introduced by spring-boot-starter-data-jpa 2.4.0 (CVE-2020-25638)
   }
 
   private fun applyDependencyManagementBom(project: Project) {
