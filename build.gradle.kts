05ad34044c66dc1629a13599ce6a2dcf56f7b1d0
Peter Phillips <peter@greenthistle.co.uk>
2021-06-28 09:39:03 +0100

    DT-2253: ⬆️ Upgrade dependencies and kotlin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,15 +2,15 @@ import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-  kotlin("jvm") version "1.5.10"
+  kotlin("jvm") version "1.5.20"
   id("com.gradle.plugin-publish") version "0.15.0"
   id("java-gradle-plugin")
   id("maven-publish")
-  id("com.github.ben-manes.versions") version "0.38.0"
-  id("se.patrikerdes.use-latest-versions") version "0.2.16"
-  id("org.owasp.dependencycheck") version "6.1.6"
+  id("com.github.ben-manes.versions") version "0.39.0"
+  id("se.patrikerdes.use-latest-versions") version "0.2.17"
+  id("org.owasp.dependencycheck") version "6.2.2"
   id("com.adarshr.test-logger") version "3.0.0"
-  id("org.jlleitschuh.gradle.ktlint") version "10.0.0"
+  id("org.jlleitschuh.gradle.ktlint") version "10.1.0"
 }
 
 repositories {
@@ -30,7 +30,7 @@ fun isNonStable(version: String): Boolean {
 }
 
 group = "uk.gov.justice.hmpps.gradle"
-version = "3.3.0"
+version = "3.3.1"
 
 gradlePlugin {
   plugins {
@@ -53,22 +53,22 @@ pluginBundle {
 dependencies {
   implementation(kotlin("reflect"))
 
-  implementation("org.springframework.boot:spring-boot-gradle-plugin:2.5.0")
-  implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.10")
+  implementation("org.springframework.boot:spring-boot-gradle-plugin:2.5.2")
+  implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.20")
   implementation("io.spring.dependency-management:io.spring.dependency-management.gradle.plugin:1.0.11.RELEASE")
-  implementation("org.owasp:dependency-check-gradle:6.1.6")
-  implementation("com.github.ben-manes:gradle-versions-plugin:0.38.0")
+  implementation("org.owasp:dependency-check-gradle:6.2.2")
+  implementation("com.github.ben-manes:gradle-versions-plugin:0.39.0")
   implementation("com.gorylenko.gradle-git-properties:com.gorylenko.gradle-git-properties.gradle.plugin:2.3.1")
   implementation("com.adarshr.test-logger:com.adarshr.test-logger.gradle.plugin:3.0.0")
-  implementation("se.patrikerdes.use-latest-versions:se.patrikerdes.use-latest-versions.gradle.plugin:0.2.16")
-  implementation("org.jlleitschuh.gradle.ktlint:org.jlleitschuh.gradle.ktlint.gradle.plugin:10.0.0")
+  implementation("se.patrikerdes.use-latest-versions:se.patrikerdes.use-latest-versions.gradle.plugin:0.2.17")
+  implementation("org.jlleitschuh.gradle.ktlint:org.jlleitschuh.gradle.ktlint.gradle.plugin:10.1.0")
 
   testImplementation("org.junit.jupiter:junit-jupiter:5.7.2")
-  testImplementation("org.mockito:mockito-junit-jupiter:3.10.0")
-  testImplementation("org.assertj:assertj-core:3.19.0")
-  testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.26.0")
+  testImplementation("org.mockito:mockito-junit-jupiter:3.11.2")
+  testImplementation("org.assertj:assertj-core:3.20.2")
+  testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.27.0")
   testImplementation("com.google.code.gson:gson:2.8.7")
-  testImplementation("org.eclipse.jgit:org.eclipse.jgit:5.11.1.202105131744-r")
+  testImplementation("org.eclipse.jgit:org.eclipse.jgit:5.12.0.202106070339-r")
 }
 
 tasks {
