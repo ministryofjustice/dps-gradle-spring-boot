9fbe7cf4f1ec45510bf471c5f1b53764cabe3e90
petergphillips <peter@greenthistle.co.uk>
2021-09-15 13:41:31 +0100

    DT-2574: 🐛 Suppress false positives for tomcat and others (#103)

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,14 +3,14 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
   kotlin("jvm") version "1.5.30"
-  id("com.gradle.plugin-publish") version "0.15.0"
+  id("com.gradle.plugin-publish") version "0.16.0"
   id("java-gradle-plugin")
   id("maven-publish")
   id("com.github.ben-manes.versions") version "0.39.0"
   id("se.patrikerdes.use-latest-versions") version "0.2.17"
-  id("org.owasp.dependencycheck") version "6.2.2"
+  id("org.owasp.dependencycheck") version "6.3.1"
   id("com.adarshr.test-logger") version "3.0.0"
-  id("org.jlleitschuh.gradle.ktlint") version "10.1.0"
+  id("org.jlleitschuh.gradle.ktlint") version "10.2.0"
 }
 
 repositories {
@@ -30,7 +30,7 @@ fun isNonStable(version: String): Boolean {
 }
 
 group = "uk.gov.justice.hmpps.gradle"
-version = "3.3.7"
+version = "3.3.8"
 
 gradlePlugin {
   plugins {
@@ -56,19 +56,19 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-gradle-plugin:2.5.4")
   implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.30")
   implementation("io.spring.dependency-management:io.spring.dependency-management.gradle.plugin:1.0.11.RELEASE")
-  implementation("org.owasp:dependency-check-gradle:6.2.2")
+  implementation("org.owasp:dependency-check-gradle:6.3.1")
   implementation("com.github.ben-manes:gradle-versions-plugin:0.39.0")
   implementation("com.gorylenko.gradle-git-properties:com.gorylenko.gradle-git-properties.gradle.plugin:2.3.1")
   implementation("com.adarshr.test-logger:com.adarshr.test-logger.gradle.plugin:3.0.0")
   implementation("se.patrikerdes.use-latest-versions:se.patrikerdes.use-latest-versions.gradle.plugin:0.2.17")
-  implementation("org.jlleitschuh.gradle.ktlint:org.jlleitschuh.gradle.ktlint.gradle.plugin:10.1.0")
+  implementation("org.jlleitschuh.gradle.ktlint:org.jlleitschuh.gradle.ktlint.gradle.plugin:10.2.0")
 
-  testImplementation("org.junit.jupiter:junit-jupiter:5.7.2")
+  testImplementation("org.junit.jupiter:junit-jupiter:5.8.0")
   testImplementation("org.mockito:mockito-junit-jupiter:3.12.4")
   testImplementation("org.assertj:assertj-core:3.20.2")
   testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.28.0")
   testImplementation("com.google.code.gson:gson:2.8.8")
-  testImplementation("org.eclipse.jgit:org.eclipse.jgit:5.12.0.202106070339-r")
+  testImplementation("org.eclipse.jgit:org.eclipse.jgit:5.13.0.202109080827-r")
 }
 
 tasks {
