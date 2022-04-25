a21463854370e7f36d497f9af9f2fb824669689d
Peter Phillips <peter@greenthistle.co.uk>
2022-04-25 08:41:48 +0100

    SDI-171: 🔒 Upgrade dependency check to prevent false positives

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
   id("maven-publish")
   id("com.github.ben-manes.versions") version "0.42.0"
   id("se.patrikerdes.use-latest-versions") version "0.2.18"
-  id("org.owasp.dependencycheck") version "7.0.4.1"
+  id("org.owasp.dependencycheck") version "7.1.0.1"
   id("com.adarshr.test-logger") version "3.0.0" // did not upgrade to 3.2.0 because experienced ListenerNotificationException - same issue as https://github.com/radarsh/gradle-test-logger-plugin/issues/241
   id("org.jlleitschuh.gradle.ktlint") version "10.2.1"
 }
@@ -30,7 +30,7 @@ fun isNonStable(version: String): Boolean {
 }
 
 group = "uk.gov.justice.hmpps.gradle"
-version = "4.1.5-beta-2"
+version = "4.1.5-beta-3"
 
 gradlePlugin {
   plugins {
@@ -56,8 +56,8 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-gradle-plugin:2.6.7")
   implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21")
   implementation("io.spring.dependency-management:io.spring.dependency-management.gradle.plugin:1.0.11.RELEASE")
-  implementation("org.owasp:dependency-check-core:7.0.4")
-  implementation("org.owasp:dependency-check-gradle:7.0.4.1")
+  implementation("org.owasp:dependency-check-core:7.1.0")
+  implementation("org.owasp:dependency-check-gradle:7.1.0.1")
   implementation("com.github.ben-manes:gradle-versions-plugin:0.42.0")
   implementation("com.gorylenko.gradle-git-properties:com.gorylenko.gradle-git-properties.gradle.plugin:2.4.0")
   implementation("com.adarshr.test-logger:com.adarshr.test-logger.gradle.plugin:3.0.0") // did not upgrade to 3.2.0 because experienced ListenerNotificationException - same issue as https://github.com/radarsh/gradle-test-logger-plugin/issues/241
@@ -65,7 +65,7 @@ dependencies {
   implementation("org.jlleitschuh.gradle.ktlint:org.jlleitschuh.gradle.ktlint.gradle.plugin:10.2.1")
 
   testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
-  testImplementation("org.mockito:mockito-junit-jupiter:4.5.0")
+  testImplementation("org.mockito:mockito-junit-jupiter:4.5.1")
   testImplementation("org.assertj:assertj-core:3.22.0")
   testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.34.0")
   testImplementation("com.google.code.gson:gson:2.9.0")
