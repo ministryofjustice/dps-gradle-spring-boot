357485d2b9890e5039b5f07187393e4c6c8dd3fe
petergphillips <peter@greenthistle.co.uk>
2022-09-07 10:49:36 +0100

    SDI-365: Suppress snakeyaml vuln (#196)

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,14 +4,14 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
   kotlin("jvm") version "1.7.10"
-  id("com.gradle.plugin-publish") version "0.21.0"
+  id("com.gradle.plugin-publish") version "1.0.0"
   id("java-gradle-plugin")
   id("maven-publish")
   id("com.github.ben-manes.versions") version "0.42.0"
   id("se.patrikerdes.use-latest-versions") version "0.2.18"
-  id("org.owasp.dependencycheck") version "7.1.1"
+  id("org.owasp.dependencycheck") version "7.1.2"
   id("com.adarshr.test-logger") version "3.0.0" // did not upgrade to 3.2.0 because experienced ListenerNotificationException - same issue as https://github.com/radarsh/gradle-test-logger-plugin/issues/241
-  id("org.jlleitschuh.gradle.ktlint") version "10.3.0"
+  id("org.jlleitschuh.gradle.ktlint") version "11.0.0"
 }
 
 repositories {
@@ -56,13 +56,13 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-gradle-plugin:2.7.3")
   implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
   implementation("io.spring.dependency-management:io.spring.dependency-management.gradle.plugin:1.0.13.RELEASE")
-  implementation("org.owasp:dependency-check-core:7.1.1")
-  implementation("org.owasp:dependency-check-gradle:7.1.1")
+  implementation("org.owasp:dependency-check-core:7.1.2")
+  implementation("org.owasp:dependency-check-gradle:7.1.2")
   implementation("com.github.ben-manes:gradle-versions-plugin:0.42.0")
   implementation("com.gorylenko.gradle-git-properties:com.gorylenko.gradle-git-properties.gradle.plugin:2.4.1")
   implementation("com.adarshr.test-logger:com.adarshr.test-logger.gradle.plugin:3.0.0") // did not upgrade to 3.2.0 because experienced ListenerNotificationException - same issue as https://github.com/radarsh/gradle-test-logger-plugin/issues/241
   implementation("se.patrikerdes.use-latest-versions:se.patrikerdes.use-latest-versions.gradle.plugin:0.2.18")
-  implementation("org.jlleitschuh.gradle.ktlint:org.jlleitschuh.gradle.ktlint.gradle.plugin:10.3.0")
+  implementation("org.jlleitschuh.gradle.ktlint:org.jlleitschuh.gradle.ktlint.gradle.plugin:11.0.0")
 
   testImplementation("org.junit.jupiter:junit-jupiter:5.9.0")
   testImplementation("org.mockito:mockito-junit-jupiter:4.7.0")
