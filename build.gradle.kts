41265b007e79e00ce3b2b16211d383d1e669604a
kth13 <kumar.thangavel@gmail.com>
2022-10-21 17:11:16 +0100

    upgrading all the plugins and dependencies to the latest

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,13 +3,13 @@ import org.gradle.configurationcache.extensions.serviceOf
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-  kotlin("jvm") version "1.7.10"
+  kotlin("jvm") version "1.7.20"
   id("com.gradle.plugin-publish") version "1.0.0"
   id("java-gradle-plugin")
   id("maven-publish")
-  id("com.github.ben-manes.versions") version "0.42.0"
+  id("com.github.ben-manes.versions") version "0.43.0"
   id("se.patrikerdes.use-latest-versions") version "0.2.18"
-  id("org.owasp.dependencycheck") version "7.2.0"
+  id("org.owasp.dependencycheck") version "7.3.0"
   id("com.adarshr.test-logger") version "3.0.0" // did not upgrade to 3.2.0 because experienced ListenerNotificationException - same issue as https://github.com/radarsh/gradle-test-logger-plugin/issues/241
   id("org.jlleitschuh.gradle.ktlint") version "11.0.0"
 }
@@ -55,19 +55,19 @@ dependencies {
 
   implementation("org.springframework.boot:spring-boot-gradle-plugin:2.7.5")
   implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
-  implementation("io.spring.dependency-management:io.spring.dependency-management.gradle.plugin:1.0.13.RELEASE")
-  implementation("org.owasp:dependency-check-core:7.2.0")
-  implementation("org.owasp:dependency-check-gradle:7.2.0")
-  implementation("com.github.ben-manes:gradle-versions-plugin:0.42.0")
+  implementation("io.spring.dependency-management:io.spring.dependency-management.gradle.plugin:1.1.0")
+  implementation("org.owasp:dependency-check-core:7.3.0")
+  implementation("org.owasp:dependency-check-gradle:7.3.0")
+  implementation("com.github.ben-manes:gradle-versions-plugin:0.43.0")
   implementation("com.gorylenko.gradle-git-properties:com.gorylenko.gradle-git-properties.gradle.plugin:2.4.1")
   implementation("com.adarshr.test-logger:com.adarshr.test-logger.gradle.plugin:3.0.0") // did not upgrade to 3.2.0 because experienced ListenerNotificationException - same issue as https://github.com/radarsh/gradle-test-logger-plugin/issues/241
   implementation("se.patrikerdes.use-latest-versions:se.patrikerdes.use-latest-versions.gradle.plugin:0.2.18")
   implementation("org.jlleitschuh.gradle.ktlint:org.jlleitschuh.gradle.ktlint.gradle.plugin:11.0.0")
 
-  testImplementation("org.junit.jupiter:junit-jupiter:5.9.0")
-  testImplementation("org.mockito:mockito-junit-jupiter:4.8.0")
+  testImplementation("org.junit.jupiter:junit-jupiter:5.9.1")
+  testImplementation("org.mockito:mockito-junit-jupiter:4.8.1")
   testImplementation("org.assertj:assertj-core:3.23.1")
-  testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.35.0")
+  testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.36.0")
   testImplementation("com.google.code.gson:gson:2.9.1")
   testImplementation("org.eclipse.jgit:org.eclipse.jgit:6.3.0.202209071007-r")
   // Had to include this when I had the same error as https://youtrack.jetbrains.com/issue/KT-49547, this links to https://github.com/gradle/gradle/issues/16774 which has includes a workaround
