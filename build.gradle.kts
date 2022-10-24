82ab7769bbee5a23c3941de4b6f9771c9e53ad77
Peter Phillips <peter@greenthistle.co.uk>
2022-10-24 09:55:20 +0100

    SDI-426: 📝 Document pinning of versions version

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
   id("com.gradle.plugin-publish") version "1.0.0"
   id("java-gradle-plugin")
   id("maven-publish")
-  id("com.github.ben-manes.versions") version "0.42.0"
+  id("com.github.ben-manes.versions")
   id("se.patrikerdes.use-latest-versions") version "0.2.18"
   id("org.owasp.dependencycheck") version "7.3.0"
   id("com.adarshr.test-logger")
@@ -54,6 +54,11 @@ pluginBundle {
 // this version is also set in settings.gradle.kts as needed by the plugins block
 val testLoggerVersion by extra("3.0.0")
 
+// Versions plugin requires gradle 7 so pin to 0.42.0 until can upgrade
+// https://github.com/ben-manes/gradle-versions-plugin/releases/tag/v0.43.0
+// this version is also set in settings.gradle.kts as needed by the plugins block
+val versionsVersion by extra("0.42.0")
+
 dependencies {
   implementation(kotlin("reflect"))
 
@@ -62,7 +67,7 @@ dependencies {
   implementation("io.spring.dependency-management:io.spring.dependency-management.gradle.plugin:1.1.0")
   implementation("org.owasp:dependency-check-core:7.3.0")
   implementation("org.owasp:dependency-check-gradle:7.3.0")
-  implementation("com.github.ben-manes:gradle-versions-plugin:0.42.0")
+  implementation("com.github.ben-manes:gradle-versions-plugin:$versionsVersion")
   implementation("com.gorylenko.gradle-git-properties:com.gorylenko.gradle-git-properties.gradle.plugin:2.4.1")
   implementation("com.adarshr.test-logger:com.adarshr.test-logger.gradle.plugin:$testLoggerVersion")
   implementation("se.patrikerdes.use-latest-versions:se.patrikerdes.use-latest-versions.gradle.plugin:0.2.18")
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,7 +4,12 @@ pluginManagement {
   // did not upgrade to 3.2.0 because experienced ListenerNotificationException - same issue as https://github.com/radarsh/gradle-test-logger-plugin/issues/241
   // this version is also set in build.gradle.kts as needed by the plugins block
   val testLoggerVersion by extra("3.0.0")
-    plugins {
-      id("com.adarshr.test-logger") version testLoggerVersion
-    }
+
+  // Versions plugin requires gradle 7 so pin to 0.42.0 until can upgrade
+  // this version is also set in build.gradle.kts as needed by the plugins block
+  val versionsVersion by extra("0.42.0")
+  plugins {
+    id("com.adarshr.test-logger") version testLoggerVersion
+    id("com.github.ben-manes.versions") version versionsVersion
+  }
 }
