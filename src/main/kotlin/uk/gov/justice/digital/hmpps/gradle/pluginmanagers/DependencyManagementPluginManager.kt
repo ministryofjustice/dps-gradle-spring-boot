802db322f6f560e4b26886ead5c5e0e8e6718e6a
Peter Phillips <peter@greenthistle.co.uk>
2022-11-02 06:54:00 +0000

    SDI-438: 🔒️ Upgrade spring security for CVE

--- src/main/kotlin/uk/gov/justice/digital/hmpps/gradle/pluginmanagers/DependencyManagementPluginManager.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/gradle/pluginmanagers/DependencyManagementPluginManager.kt
@@ -12,8 +12,10 @@ class DependencyManagementPluginManager(override val project: Project) : PluginM
     applyDependencyManagementBom(project)
     // overriding snakeyaml for https://nvd.nist.gov/vuln/detail/CVE-2022-25857 - brought in by org.springframework.boot:spring-boot-dependencies:2.7.2
     // overriding snakeyaml for https://nvd.nist.gov/vuln/detail/CVE-2022-38751 - brought in by org.springframework.boot:spring-boot-dependencies:2.7.2
-    project.extensions.extraProperties["snakeyaml.version"] = "1.32"
+    project.extensions.extraProperties["snakeyaml.version"] = "1.33"
 
+    // Upgrading to 5.7.5 - remove when Spring Boot upgrades to > 2.7.5 (along with tests in functional/DependencyManagementPluginManagerTest)
+    project.extensions.extraProperties["spring-security.version"] = "5.7.5"
   }
 
   private fun applyDependencyManagementBom(project: Project) {
