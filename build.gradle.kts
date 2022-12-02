f2dc3bf7b7e97c57a28d0187bf4b77cecfaae802
Peter Phillips <peter@greenthistle.co.uk>
2022-12-02 23:28:05 +0000

    SDI-477: ⬆ Bump versions and remove pinning

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,10 +7,10 @@ plugins {
   id("com.gradle.plugin-publish") version "1.1.0"
   id("java-gradle-plugin")
   id("maven-publish")
-  id("com.github.ben-manes.versions")
+  id("com.github.ben-manes.versions") version "0.44.0"
   id("se.patrikerdes.use-latest-versions") version "0.2.18"
   id("org.owasp.dependencycheck") version "7.3.2"
-  id("com.adarshr.test-logger")
+  id("com.adarshr.test-logger") version "3.2.0"
   id("org.jlleitschuh.gradle.ktlint") version "11.0.0"
 }
 
@@ -50,10 +50,6 @@ pluginBundle {
   tags = listOf("hmpps", "spring-boot")
 }
 
-// did not upgrade to 3.2.0 because experienced ListenerNotificationException - same issue as https://github.com/radarsh/gradle-test-logger-plugin/issues/241
-// this version is also set in settings.gradle.kts as needed by the plugins block
-val testLoggerVersion by extra("3.2.0")
-
 dependencies {
   implementation(kotlin("reflect"))
 
@@ -64,7 +60,7 @@ dependencies {
   implementation("org.owasp:dependency-check-gradle:7.3.2")
   implementation("com.github.ben-manes:gradle-versions-plugin:0.44.0")
   implementation("com.gorylenko.gradle-git-properties:com.gorylenko.gradle-git-properties.gradle.plugin:2.4.1")
-  implementation("com.adarshr.test-logger:com.adarshr.test-logger.gradle.plugin:$testLoggerVersion")
+  implementation("com.adarshr.test-logger:com.adarshr.test-logger.gradle.plugin:3.2.0")
   implementation("se.patrikerdes.use-latest-versions:se.patrikerdes.use-latest-versions.gradle.plugin:0.2.18")
   implementation("org.jlleitschuh.gradle.ktlint:org.jlleitschuh.gradle.ktlint.gradle.plugin:11.0.0")
 
@@ -73,7 +69,7 @@ dependencies {
   testImplementation("org.assertj:assertj-core:3.23.1")
   testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.36.0")
   testImplementation("com.google.code.gson:gson:2.10")
-  testImplementation("org.eclipse.jgit:org.eclipse.jgit:6.3.0.202209071007-r")
+  testImplementation("org.eclipse.jgit:org.eclipse.jgit:6.4.0.202211300538-r")
   // Had to include this when I had the same error as https://youtrack.jetbrains.com/issue/KT-49547, this links to https://github.com/gradle/gradle/issues/16774 which has includes a workaround
   testRuntimeOnly(
     files(
--- src/main/kotlin/uk/gov/justice/digital/hmpps/gradle/pluginmanagers/VersionsPluginManager.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/gradle/pluginmanagers/VersionsPluginManager.kt
@@ -20,7 +20,7 @@ class VersionsPluginManager(override val project: Project) : PluginManager<Versi
   }
 
   private fun isStable(version: String): Boolean {
-    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.toUpperCase().contains(it) }
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
     val regex = "^[0-9,.v-]+(-r)?$".toRegex()
     return stableKeyword || regex.matches(version)
   }
