03275169970185d40372ed9596834bea712ff257
petergphillips <peter@greenthistle.co.uk>
2022-12-05 15:27:09 +0000

    SDI-477: ✅ Replace unit test with functional test and fix ktlint (#219)

--- src/test/kotlin/uk/gov/justice/digital/hmpps/gradle/pluginmanagers/DependencyManagementPluginManagerTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/gradle/pluginmanagers/DependencyManagementPluginManagerTest.kt
@@ -1,23 +1,29 @@
 package uk.gov.justice.digital.hmpps.gradle.pluginmanagers
 
-import org.junit.jupiter.api.Test
 import org.assertj.core.api.Assertions.assertThat
 import org.gradle.testkit.runner.TaskOutcome
-import uk.gov.justice.digital.hmpps.gradle.functional.*
-
+import org.junit.jupiter.api.Test
+import uk.gov.justice.digital.hmpps.gradle.functional.GradleBuildTest
+import uk.gov.justice.digital.hmpps.gradle.functional.buildProject
+import uk.gov.justice.digital.hmpps.gradle.functional.findJar
+import uk.gov.justice.digital.hmpps.gradle.functional.javaProjectDetails
+import uk.gov.justice.digital.hmpps.gradle.functional.makeProject
 import java.util.jar.JarFile
+
 class DependencyManagementPluginManagerTest : GradleBuildTest() {
 
   @Test
   fun `Wrong transitive version of snakeyaml should be overridden by the plugin`() {
-    val project = javaProjectDetails(projectDir).copy(buildScript = """
+    val project = javaProjectDetails(projectDir).copy(
+      buildScript = """
         plugins {
           id("uk.gov.justice.hmpps.gradle-spring-boot") version "0.1.0"
         }
         dependencies {
             implementation("org.springframework.boot:spring-boot-starter-web");
         }
-    """.trimIndent())
+      """.trimIndent()
+    )
     makeProject(project)
 
     val result = buildProject(projectDir, "bootJar")
@@ -28,10 +34,10 @@ class DependencyManagementPluginManagerTest : GradleBuildTest() {
     assertThat(jarContains(jarFile, "snakeyaml-1.33")).isTrue
   }
 
-
   @Test
   fun `Upgrade of spring security should be overridden by the plugin`() {
-    val project = javaProjectDetails(projectDir).copy(buildScript = """
+    val project = javaProjectDetails(projectDir).copy(
+      buildScript = """
         plugins {
           id("uk.gov.justice.hmpps.gradle-spring-boot") version "0.1.0"
         }
@@ -39,7 +45,8 @@ class DependencyManagementPluginManagerTest : GradleBuildTest() {
           implementation("org.springframework.boot:spring-boot-starter-security");
           implementation("org.springframework.boot:spring-boot-starter-oauth2-client");
         }
-    """.trimIndent())
+      """.trimIndent()
+    )
     makeProject(project)
 
     val result = buildProject(projectDir, "bootJar")
@@ -61,4 +68,3 @@ class DependencyManagementPluginManagerTest : GradleBuildTest() {
   private fun jarContains(jar: JarFile, dependency: String): Boolean =
     jar.getJarEntry("BOOT-INF/lib/$dependency.jar") != null
 }
-
