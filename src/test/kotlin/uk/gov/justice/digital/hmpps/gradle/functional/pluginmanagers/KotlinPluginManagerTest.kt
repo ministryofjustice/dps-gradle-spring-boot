7c383033b673f6dcfae3f046c693968af30001c3
Peter Phillips <peter@greenthistle.co.uk>
2022-12-05 14:35:41 +0000

    SDI-477: ✅ Replace unit test with functional test and fix ktlint

--- src/test/kotlin/uk/gov/justice/digital/hmpps/gradle/functional/pluginmanagers/KotlinPluginManagerTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/gradle/functional/pluginmanagers/KotlinPluginManagerTest.kt
@@ -1,6 +1,6 @@
 package uk.gov.justice.digital.hmpps.gradle.functional.pluginmanagers
 
-import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
 import org.gradle.testkit.runner.TaskOutcome
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
@@ -8,6 +8,7 @@ import org.junit.jupiter.params.provider.MethodSource
 import uk.gov.justice.digital.hmpps.gradle.functional.GradleBuildTest
 import uk.gov.justice.digital.hmpps.gradle.functional.ProjectDetails
 import uk.gov.justice.digital.hmpps.gradle.functional.buildProject
+import uk.gov.justice.digital.hmpps.gradle.functional.getDependencyVersion
 import uk.gov.justice.digital.hmpps.gradle.functional.kotlinProjectDetails
 import uk.gov.justice.digital.hmpps.gradle.functional.makeProject
 
@@ -18,6 +19,15 @@ class KotlinPluginManagerTest : GradleBuildTest() {
     fun kotlinOnlyProject() = listOf(
       Arguments.of(kotlinProjectDetails(projectDir))
     )
+
+    @JvmStatic
+    fun projectDetailsWithMockitoDependency() = listOf(
+      Arguments.of(
+        kotlinProjectDetails(projectDir).copy(
+          testClass = kotlinMockitoTest(),
+        )
+      )
+    )
   }
 
   @ParameterizedTest
@@ -27,6 +37,52 @@ class KotlinPluginManagerTest : GradleBuildTest() {
 
     // This compile would fail at JVM 1.6 which is the default
     val result = buildProject(projectDir, "compileKotlin")
-    Assertions.assertThat(result.task(":compileKotlin")?.outcome).isEqualTo(TaskOutcome.SUCCESS)
+    assertThat(result.task(":compileKotlin")?.outcome).isEqualTo(TaskOutcome.SUCCESS)
+  }
+
+  @ParameterizedTest
+  @MethodSource("kotlinOnlyProject")
+  fun `Adds in the jackson module kotlin`(projectDetails: ProjectDetails) {
+    makeProject(projectDetails)
+
+    val webVersion = getDependencyVersion(projectDir, "com.fasterxml.jackson.module:jackson-module-kotlin")
+    assertThat(webVersion).isNotBlank
+  }
+
+  @ParameterizedTest
+  @MethodSource("kotlinOnlyProject")
+  fun `Adds in kotlin reflect`(projectDetails: ProjectDetails) {
+    makeProject(projectDetails)
+
+    val webVersion = getDependencyVersion(projectDir, "org.jetbrains.kotlin:kotlin-reflect")
+    assertThat(webVersion).isNotBlank
+  }
+
+  @ParameterizedTest
+  @MethodSource("projectDetailsWithMockitoDependency")
+  fun `Adds in mockito kotlin`(projectDetails: ProjectDetails) {
+    makeProject(projectDetails)
+
+    val result = buildProject(projectDir, "compileTestKotlin")
+    assertThat(result.output)
+      .contains("Task :compileTestKotlin")
+      .contains("BUILD SUCCESSFUL")
   }
 }
+
+private fun kotlinMockitoTest(): String = """
+    package uk.gov.justice.digital.hmpps.app
+
+    import org.assertj.core.api.Assertions.assertThat
+    import org.junit.Test
+    import org.mockito.kotlin.mock
+
+    class ApplicationTest {
+      private val bob: String = mock()
+
+      @Test
+      fun `A Test`() {
+        assertThat("anything").isEqualTo("anything")
+      }
+    }
+""".trimIndent()
