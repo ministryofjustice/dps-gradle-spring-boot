8fd2087d904e1fa3ebea57374b5841c54ec52632
petergphillips <peter@greenthistle.co.uk>
2023-01-10 10:47:42 +0000

    SDI-528: ⬆ Bump owasp for database column error (#234)
    * SDI-528: ⬆  Bump owasp for database column error
    
    * SDI-528: 🔒️ Also suppress new snakeyaml vulns

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
   id("maven-publish")
   id("com.github.ben-manes.versions") version "0.44.0"
   id("se.patrikerdes.use-latest-versions") version "0.2.18"
-  id("org.owasp.dependencycheck") version "7.4.3"
+  id("org.owasp.dependencycheck") version "7.4.4"
   id("com.adarshr.test-logger") version "3.2.0"
   id("org.jlleitschuh.gradle.ktlint") version "11.0.0"
 }
@@ -30,7 +30,7 @@ fun isNonStable(version: String): Boolean {
 }
 
 group = "uk.gov.justice.hmpps.gradle"
-version = "5.0.0-beta-3"
+version = "5.0.0-beta-4"
 
 gradlePlugin {
   plugins {
@@ -56,8 +56,8 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-gradle-plugin:3.0.1")
   implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.0")
   implementation("io.spring.dependency-management:io.spring.dependency-management.gradle.plugin:1.1.0")
-  implementation("org.owasp:dependency-check-core:7.4.3")
-  implementation("org.owasp:dependency-check-gradle:7.4.3")
+  implementation("org.owasp:dependency-check-core:7.4.4")
+  implementation("org.owasp:dependency-check-gradle:7.4.4")
   implementation("com.github.ben-manes:gradle-versions-plugin:0.44.0")
   implementation("com.gorylenko.gradle-git-properties:com.gorylenko.gradle-git-properties.gradle.plugin:2.4.1")
   implementation("com.adarshr.test-logger:com.adarshr.test-logger.gradle.plugin:3.2.0")
@@ -66,9 +66,9 @@ dependencies {
 
   testImplementation("org.junit.jupiter:junit-jupiter:5.9.1")
   testImplementation("org.mockito:mockito-junit-jupiter:4.11.0")
-  testImplementation("org.assertj:assertj-core:3.23.1")
+  testImplementation("org.assertj:assertj-core:3.24.1")
   testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.36.0")
-  testImplementation("com.google.code.gson:gson:2.10")
+  testImplementation("com.google.code.gson:gson:2.10.1")
   testImplementation("org.eclipse.jgit:org.eclipse.jgit:6.4.0.202211300538-r")
   // Had to include this when I had the same error as https://youtrack.jetbrains.com/issue/KT-49547, this links to https://github.com/gradle/gradle/issues/16774 which has includes a workaround
   testRuntimeOnly(
