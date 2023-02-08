ddfa25608bffa9f7046658a40a85d24cce118365
Peter Phillips <peter@greenthistle.co.uk>
2023-02-08 10:51:12 +0000

    SDI-477: ⬆  Bump to kotlin 1.8.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.gradle.configurationcache.extensions.serviceOf
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-  kotlin("jvm") version "1.8.0"
+  kotlin("jvm") version "1.8.10"
   id("com.gradle.plugin-publish") version "1.1.0"
   id("java-gradle-plugin")
   id("maven-publish")
@@ -30,7 +30,7 @@ fun isNonStable(version: String): Boolean {
 }
 
 group = "uk.gov.justice.hmpps.gradle"
-version = "5.0.1"
+version = "5.0.2"
 
 gradlePlugin {
   plugins {
@@ -54,7 +54,7 @@ dependencies {
   implementation(kotlin("reflect"))
 
   implementation("org.springframework.boot:spring-boot-gradle-plugin:3.0.2")
-  implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.0")
+  implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.10")
   implementation("io.spring.dependency-management:io.spring.dependency-management.gradle.plugin:1.1.0")
   implementation("org.owasp:dependency-check-core:7.4.4")
   implementation("org.owasp:dependency-check-gradle:7.4.4")
