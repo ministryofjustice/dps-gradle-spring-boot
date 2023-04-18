2eb425436cc187c07070c18a958b9a648a474050
Peter Phillips <peter@greenthistle.co.uk>
2023-04-18 10:40:24 +0100

    SDIT-747: 🔒️ Upgrade spring framework and remove pinning of netty and spring security

--- README.md
+++ README.md
@@ -9,6 +9,7 @@ This Gradle plugin is used to orchestrate DPS Spring Boot projects such that:
 * CVEs causing `dependencyCheckAnalyze` failures are mitigated in a single place rather than in each and every project
 
 ## Release Notes
+##### [4.8.6](release-notes/4.8.6.md)
 ##### [4.8.5](release-notes/4.8.5.md)
 ##### [4.8.4](release-notes/4.8.4.md)
 ##### [4.8.3](release-notes/4.8.3.md)
@@ -129,7 +130,7 @@ In your `build.gradle.kts` (or `build.gradle` for Java) add the following line t
 ```
 plugins {
   ...
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.8.5"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.8.6"
   ...
 }
 ```
--- src/test/kotlin/uk/gov/justice/digital/hmpps/gradle/pluginmanagers/DependencyManagementPluginManagerSpringFrameworkTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/gradle/pluginmanagers/DependencyManagementPluginManagerSpringFrameworkTest.kt
@@ -15,22 +15,19 @@ import uk.gov.justice.digital.hmpps.gradle.functional.makeProject
 import java.util.jar.JarFile
 import kotlin.streams.toList
 
-class DependencyManagementPluginManagerNettyTest : GradleBuildTest() {
-
+class DependencyManagementPluginManagerSpringFrameworkTest : GradleBuildTest() {
   companion object {
     @Suppress("unused")
     @JvmStatic
-    fun wrongTransitiveNettyVersion() = listOf(
-      Arguments.of(javaProjectDetails(projectDir).copy(buildScript = wrongTransitiveNettyVersionBuildFile())),
-      Arguments.of(kotlinProjectDetails(projectDir).copy(buildScript = wrongTransitiveNettyVersionBuildFile())),
+    fun wrongTransitiveSpringFrameworkVersion() = listOf(
+      Arguments.of(javaProjectDetails(projectDir).copy(buildScript = wrongTransitiveSpringFrameworkVersionBuildFile())),
+      Arguments.of(kotlinProjectDetails(projectDir).copy(buildScript = wrongTransitiveSpringFrameworkVersionBuildFile())),
     )
   }
-  private fun jarContainsNettyCommon(jar: JarFile, version: String): Boolean =
-    jar.getJarEntry("BOOT-INF/lib/netty-common-$version.jar") != null
 
   @ParameterizedTest
-  @MethodSource("wrongTransitiveNettyVersion")
-  fun `Wrong transitive version of netty should be overridden by the plugin`(projectDetails: ProjectDetails) {
+  @MethodSource("wrongTransitiveSpringFrameworkVersion")
+  fun `Wrong transitive version of SpringFramework should be overridden by the plugin`(projectDetails: ProjectDetails) {
     makeProject(projectDetails.copy())
 
     val result = buildProject(projectDir, "bootJar")
@@ -39,16 +36,16 @@ class DependencyManagementPluginManagerNettyTest : GradleBuildTest() {
     val file = findJar(projectDir, projectDetails.projectName)
     val jarContents = JarFile(file).versionedStream().map { it.name }.toList()
     assertThat(jarContents)
-      .doesNotContain("BOOT-INF/lib/netty-common-4.1.85.Final.jar")
-      .contains("BOOT-INF/lib/netty-common-4.1.86.Final.jar")
+      .doesNotContain("BOOT-INF/lib/spring-core-5.3.26.jar")
+      .contains("BOOT-INF/lib/spring-core-5.3.27.jar")
   }
 }
 
-private fun wrongTransitiveNettyVersionBuildFile() = """
+private fun wrongTransitiveSpringFrameworkVersionBuildFile() = """
     plugins {
       id("uk.gov.justice.hmpps.gradle-spring-boot") version "0.1.0"
     }
     dependencies {
-      implementation("org.springframework.boot:spring-boot-starter-webflux")
+      implementation("org.springframework.boot:spring-boot-starter-web")
     }
 """.trimIndent()
