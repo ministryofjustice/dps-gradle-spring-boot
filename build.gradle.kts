2c56c514031fafb1f7b0962dc23eef68cf21303e
Mike Halma <mike.halma@digital.justice.gov.uk>
2023-07-20 15:00:37 +0100

    SDIT-945 beta

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,7 @@ fun isNonStable(version: String): Boolean {
 }
 
 group = "uk.gov.justice.hmpps.gradle"
-version = "5.3.0"
+version = "5.3.0-beta"
 
 gradlePlugin {
   website.set("https://github.com/ministryofjustice/dps-gradle-spring-boot")
