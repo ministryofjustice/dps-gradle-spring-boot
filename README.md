ef6e05dc0a77000317464bd210e66e2e462d931d
Peter Phillips <peter@greenthistle.co.uk>
2023-10-19 15:07:56 +0100

    SDIT-1164: 🔒️ Upgrade to 3.1.5 for tomcat vulnerabilities

--- README.md
+++ README.md
@@ -9,6 +9,7 @@ This Gradle plugin is used to orchestrate DPS Spring Boot projects such that:
 * CVEs causing `dependencyCheckAnalyze` failures are mitigated in a single place rather than in each and every project
 
 ## Release Notes
+##### [5.7.0](release-notes/5.7.0.md)
 ##### [5.6.0](release-notes/5.6.0.md)
 ##### [5.5.1](release-notes/5.5.1.md)
 ##### [5.5.0](release-notes/5.5.0.md)
@@ -160,7 +161,7 @@ In your `build.gradle.kts` (or `build.gradle` for Java) add the following line t
 ```
 plugins {
   ...
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.6.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.7.0"
   ...
 }
 ```
--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
   id("com.github.ben-manes.versions") version "0.49.0"
   id("se.patrikerdes.use-latest-versions") version "0.2.18"
   id("org.owasp.dependencycheck") version "8.4.0"
-  id("com.adarshr.test-logger") version "3.2.0"
+  id("com.adarshr.test-logger") version "4.0.0"
   id("org.jlleitschuh.gradle.ktlint") version "11.6.1"
 }
 
@@ -30,7 +30,7 @@ fun isNonStable(version: String): Boolean {
 }
 
 group = "uk.gov.justice.hmpps.gradle"
-version = "5.6.0"
+version = "5.7.0"
 
 gradlePlugin {
   website.set("https://github.com/ministryofjustice/dps-gradle-spring-boot")
@@ -50,14 +50,14 @@ gradlePlugin {
 dependencies {
   implementation(kotlin("reflect"))
 
-  implementation("org.springframework.boot:spring-boot-gradle-plugin:3.1.4")
+  implementation("org.springframework.boot:spring-boot-gradle-plugin:3.1.5")
   implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.22")
   implementation("io.spring.dependency-management:io.spring.dependency-management.gradle.plugin:1.1.3")
   implementation("org.owasp:dependency-check-core:8.4.0")
   implementation("org.owasp:dependency-check-gradle:8.4.0")
   implementation("com.github.ben-manes:gradle-versions-plugin:0.49.0")
   implementation("com.gorylenko.gradle-git-properties:com.gorylenko.gradle-git-properties.gradle.plugin:2.4.1")
-  implementation("com.adarshr.test-logger:com.adarshr.test-logger.gradle.plugin:3.2.0")
+  implementation("com.adarshr.test-logger:com.adarshr.test-logger.gradle.plugin:4.0.0")
   implementation("se.patrikerdes.use-latest-versions:se.patrikerdes.use-latest-versions.gradle.plugin:0.2.18")
   implementation("org.jlleitschuh.gradle.ktlint:org.jlleitschuh.gradle.ktlint.gradle.plugin:11.6.1")
 
