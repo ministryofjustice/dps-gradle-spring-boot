acd938732a435495a6a867acc928c5a45041230a
Andy Aspell-Clark <120116700+andyaspellclark-moj@users.noreply.github.com>
2023-12-06 15:57:35 +0000

    updated the `dps-gradle-spring-boot` version to 5.11.0 updated the owasp dependency checker version to 9.0.3 updated the release notes

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
   id("maven-publish")
   id("com.github.ben-manes.versions") version "0.50.0"
   id("se.patrikerdes.use-latest-versions") version "0.2.18"
-  id("org.owasp.dependencycheck") version "9.0.2"
+  id("org.owasp.dependencycheck") version "9.0.3"
   id("com.adarshr.test-logger") version "4.0.0"
   id("org.jlleitschuh.gradle.ktlint") version "11.6.1"
 }
@@ -30,7 +30,7 @@ fun isNonStable(version: String): Boolean {
 }
 
 group = "uk.gov.justice.hmpps.gradle"
-version = "5.10.2"
+version = "5.11.0"
 
 gradlePlugin {
   website.set("https://github.com/ministryofjustice/dps-gradle-spring-boot")
@@ -53,8 +53,8 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-gradle-plugin:3.2.0")
   implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.22")
   implementation("io.spring.dependency-management:io.spring.dependency-management.gradle.plugin:1.1.4")
-  implementation("org.owasp:dependency-check-core:9.0.2")
-  implementation("org.owasp:dependency-check-gradle:9.0.2")
+  implementation("org.owasp:dependency-check-core:9.0.3")
+  implementation("org.owasp:dependency-check-gradle:9.0.3")
   implementation("com.github.ben-manes:gradle-versions-plugin:0.50.0")
   implementation("com.gorylenko.gradle-git-properties:com.gorylenko.gradle-git-properties.gradle.plugin:2.4.1")
   implementation("com.adarshr.test-logger:com.adarshr.test-logger.gradle.plugin:4.0.0")
