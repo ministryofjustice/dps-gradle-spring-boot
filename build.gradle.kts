c18a853cbc4dc0f764e9b5539021f96ad921d657
simonmitchell <simon.mitchell@digital.justice.gov.uk>
2024-01-03 12:49:52 +0000

    HAAR-2154: update release note

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,7 @@ fun isNonStable(version: String): Boolean {
 }
 
 group = "uk.gov.justice.hmpps.gradle"
-version = "4.14.0-beta"
+version = "4.14.0"
 
 gradlePlugin {
   website.set("https://github.com/ministryofjustice/dps-gradle-spring-boot")
