7e4c274fc03f0ac4f51a6f6fd61766c7937cd082
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2024-08-22 20:09:11 +0100

    SDIT-1988: ⬆️ Upgrade to spring boot 3.3.3 and kotlin 2.0.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-  kotlin("jvm") version "2.0.10"
+  kotlin("jvm") version "2.0.20"
   id("com.gradle.plugin-publish") version "1.2.1"
   id("java-gradle-plugin")
   id("maven-publish")
@@ -30,7 +30,7 @@ fun isNonStable(version: String): Boolean {
 }
 
 group = "uk.gov.justice.hmpps.gradle"
-version = "6.0.3"
+version = "6.0.4"
 
 gradlePlugin {
   website.set("https://github.com/ministryofjustice/dps-gradle-spring-boot")
@@ -50,7 +50,7 @@ gradlePlugin {
 dependencies {
   implementation(kotlin("reflect"))
 
-  implementation("org.springframework.boot:spring-boot-gradle-plugin:3.3.2")
+  implementation("org.springframework.boot:spring-boot-gradle-plugin:3.3.3")
   implementation(kotlin("gradle-plugin"))
   implementation("io.spring.dependency-management:io.spring.dependency-management.gradle.plugin:1.1.6")
   implementation("org.owasp:dependency-check-core:8.4.3")
