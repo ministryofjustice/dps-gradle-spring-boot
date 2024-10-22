c048792b3f121a8c00597d1f866b0df9f8053329
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2024-10-22 09:41:53 +0100

    SDIT-2141: 🔒️ Suppress path directory traveral attack

--- src/main/kotlin/uk/gov/justice/digital/hmpps/gradle/configmanagers/AppInsightsConfigManager.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/gradle/configmanagers/AppInsightsConfigManager.kt
@@ -4,8 +4,8 @@ import org.gradle.api.Project
 import org.gradle.api.tasks.Copy
 import uk.gov.justice.digital.hmpps.gradle.ConfigManager
 
-private const val APP_INSIGHTS_VERSION = "3.5.4"
-const val OPENTELEMETRY_VERSION = "1.40.0"
+private const val APP_INSIGHTS_VERSION = "3.6.1"
+const val OPENTELEMETRY_VERSION = "1.43.0"
 
 class AppInsightsConfigManager(override val project: Project) : ConfigManager {
   override fun configure() {
