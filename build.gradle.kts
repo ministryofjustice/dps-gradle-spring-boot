f4aa4fe3edc3bfcbcba95ab5ba8f62adee359380
petergphillips <5099053+petergphillips@users.noreply.github.com>
2024-11-19 14:19:15 +0000

    SDIT-2265: 🔒️ Pin upgrade of netty for security vuln (#346)

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,7 @@ fun isNonStable(version: String): Boolean {
 }
 
 group = "uk.gov.justice.hmpps.gradle"
-version = "6.0.8"
+version = "6.0.9"
 
 gradlePlugin {
   website.set("https://github.com/ministryofjustice/dps-gradle-spring-boot")
