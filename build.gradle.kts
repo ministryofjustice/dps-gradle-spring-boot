61878419ccc60b58a5b248dee6e942b03339a748
petergphillips <5099053+petergphillips@users.noreply.github.com>
2024-12-30 11:06:59 +0000

    SDIT-2362: 🔒️ Suppress logback & tomcat security issues (#349)

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,7 @@ fun isNonStable(version: String): Boolean {
 }
 
 group = "uk.gov.justice.hmpps.gradle"
-version = "6.1.1"
+version = "6.1.2"
 
 gradlePlugin {
   website.set("https://github.com/ministryofjustice/dps-gradle-spring-boot")
