257971190c93e086bac45192520c81bf808581e7
Mike Halma <mike.halma@digital.justice.gov.uk>
2025-01-24 08:15:23 +0000

    SDIT-2488 Upgrade to Spring Boot 3.4.2

--- src/main/kotlin/uk/gov/justice/digital/hmpps/gradle/configmanagers/AppInsightsConfigManager.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/gradle/configmanagers/AppInsightsConfigManager.kt
@@ -5,7 +5,7 @@ import org.gradle.api.tasks.Copy
 import uk.gov.justice.digital.hmpps.gradle.ConfigManager
 
 private const val APP_INSIGHTS_VERSION = "3.6.2"
-const val OPENTELEMETRY_VERSION = "1.43.0"
+const val OPENTELEMETRY_VERSION = "1.46.0"
 
 class AppInsightsConfigManager(override val project: Project) : ConfigManager {
   override fun configure() {
