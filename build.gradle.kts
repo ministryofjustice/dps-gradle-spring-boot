4cac6bf33b81c7e14bf6dad3e41cade413e49b69
Mike Halma <58170926+mikehalmamoj@users.noreply.github.com>
2025-07-17 08:52:28 +0100

    SDIT-2948 Add mitigation for CVE-2025-53864, CVE-2025-48924 and CVE-2025-22227 (#371)

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,7 +31,7 @@ fun isNonStable(version: String): Boolean {
 }
 
 group = "uk.gov.justice.hmpps.gradle"
-version = "8.3.1"
+version = "8.3.2"
 
 gradlePlugin {
   website.set("https://github.com/ministryofjustice/hmpps-gradle-spring-boot")
--- src/main/kotlin/uk/gov/justice/digital/hmpps/gradle/pluginmanagers/DependencyManagementPluginManager.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/gradle/pluginmanagers/DependencyManagementPluginManager.kt
@@ -14,6 +14,7 @@ class DependencyManagementPluginManager(override val project: Project) : PluginM
   override fun configure() {
     applyDependencyManagementBom(project)
     project.extensions.extraProperties["opentelemetry.version"] = OPENTELEMETRY_VERSION
+    project.extensions.extraProperties["reactor-bom.version"] = "2024.0.8"
   }
 
   private fun applyDependencyManagementBom(project: Project) {
